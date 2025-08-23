360b7f7d3099c256ee8a7c879593c2a165f705ac
Jan Rehorik <jrehorik@serviceobjects.com>
2025-08-22 17:31:56 -0700

    Typo

--- address-geocode-international-dot-net-examples/Main.cs
+++ address-geocode-international-dot-net-examples/Main.cs
@@ -4,7 +4,7 @@ using address_geocode_international_dot_net_examples;
 //Your license key from Service Objects.
 //Trial license keys will only work on the
 //trail environments and production license
-//keys will only owork on production environments.
+//keys will only work on production environments.
 string LicenseKey = "LICENSE KEY";
 
 bool IsProductionKey = false;
